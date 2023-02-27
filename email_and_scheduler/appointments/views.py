101e00721d50d6024f5e57c389729eac45655ca6
Ilya Mikhasik <imikhassik@gmail.com>
2023-02-27 05:29:40 -0500

    Add AppointmentView, template and urlpattern

--- email_and_scheduler/appointments/views.py
+++ email_and_scheduler/appointments/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect
+from django.views import View
+from django.core.mail import send_mail
+from datetime import datetime
 
-# Create your views here.
+from .models import Appointment
+
+
+class AppointmentView(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'make_appointment.html', {})
+
+    def post(self, request, *args, **kwargs):
+        appointment = Appointment(
+            date=datetime.strptime(request.POST['date'], '%Y-%m-%d'),
+            client_name=request.POST['client_name'],
+            message=request.POST['message'],
+        )
+        appointment.save()
+
+        return redirect('appointments:make_appointment')
