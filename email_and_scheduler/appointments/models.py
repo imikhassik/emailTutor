49885e1c7c0d3310740e9ed2f271ef050b26fb1a
Ilya Mikhasik <imikhassik@gmail.com>
2023-02-27 02:45:34 -0500

    Add Appointment model

--- email_and_scheduler/appointments/models.py
+++ email_and_scheduler/appointments/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Appointment(models.Model):
+    date = models.DateField(default=datetime.utcnow)
+    client_name = models.CharField(max_length=200)
+    message = models.TextField()
+
+    def __str__(self):
+        return f"{self.client_name}: {self.message}"
